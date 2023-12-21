5e17726e1e50393b0095b21d8c5133118e1a2770
Deekshithanagaraj <deekshithangowda06@gmail.com>
2023-12-21 12:06:02 +0800

    This is the initial commit for the TurnUp automation

---
+++ Turn_Up_Portal/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+//Open the chrome browser
+IWebDriver driver = new ChromeDriver();
+//Maximize the browser
+driver.Manage().Window.Maximize();
+
+//Launch TurnUp protal and nevigate to login page 
+driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+//Identify username textbook and enter valid username
+IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+usernameTextBox.SendKeys("hari");
+//Identify password textbook and enter valid password
+IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+passwordTextBox.SendKeys("123123");
+
+//Identify the login button and click on the button
+IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\'loginForm\']/form/div[3]/input[1]"));
+loginButton.Click();
+
+//check if user has logged in successfully
+IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/ul/li[1]/a"));
+if (helloHari.Text == "Hello hari!")
+{
+    Console.WriteLine("User has logged in successfully");
+}
+else
+{
+    Console.WriteLine("User hasn't been logged in.");
+}
